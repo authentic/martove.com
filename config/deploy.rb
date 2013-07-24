551207fbd36651c4cf717b3d4b40f3ca5850945a
authentic <galaxy73rambler@yahoo.com>
2013-07-24 16:47:39 -0400

    deploy fixes

---
+++ config/deploy.rb
@@ -0,0 +1,82 @@
+require "bundler/capistrano"
+
+
+#############################################################
+#	Application
+#############################################################
+set :application, "martove"
+set :deploy_to, "/home/massanut/#{application}"
+
+#############################################################
+#	Settings
+#############################################################
+
+default_run_options[:pty] = true
+set :use_sudo, false
+ssh_options[:port] = 7822
+ssh_options[:keys] = %w(~/.ssh/id_rsa)
+set :scm_verbose, true
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "massanut"
+set :domain, "massanut.www76.a2hosting.com"
+role :web, domain
+role :app, domain
+role :db, domain, :primary => true
+set :bundle_cmd, 'source $HOME/.bash_profile && bundle'
+
+#############################################################
+#	Git
+#############################################################
+set :repository, "git://github.com/authentic/martove.com.git"
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+after 'deploy', 'deploy:cleanup'
+
+namespace :deploy do
+#############################################################
+#	Passenger
+#############################################################
+  task :start do
+    ;
+  end
+  task :stop do
+    ;
+  end
+  task :restart, :role => :app, :except => {:no_release => true} do
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+#############################################################
+#	Symlink tasks
+#############################################################
+  desc "Symlinks the database.yml, and system folders"
+  task :symlink_config_shared, :role => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -s #{shared_path}/refinery_assets #{release_path}/public/system"
+
+  end
+
+  desc "Creates the production log if it does not exist"
+  task :create_production_log do
+    unless File.exist?(File.join(shared_path, 'log', 'production.log'))
+      puts "\n\n=== Creating Production Log! ===\n\n"
+      run "touch #{File.join(shared_path, 'log', 'production.log')}"
+    end
+  end
+#desc "build missing paperclip styles"
+#task :build_missing_paperclip_styles, :roles => :app do
+#  run "cd #{release_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+#end
+#
+#desc "Sync the public/ckeditor_assets directory."
+#task :assets do
+#  system "rsync -vr --exclude='.DS_Store' public/ckeditor_assets #{user}@#{application}:#{shared_path}/"
+#end
+end
+after 'deploy:update_code', 'deploy:symlink_config_shared' #, 'create_production_log'
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,17 +5,17 @@ MartoveCom::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -24,17 +24,17 @@ MartoveCom::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  #config.log_level = :debug
 
   # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
+  #config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
@@ -64,4 +64,5 @@ MartoveCom::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.precompile += ['admin.js', 'calendar.js', 'print.css', 'calendar.css', 'mercury.css', 'jquery-1.7.js', 'mercury_override.js', 'mercury.js']
 end
