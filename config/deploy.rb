73921fa43cf48cd034c3729a5cdd920ee82b84cd
authentic <galaxy73rambler@yahoo.com>
2013-07-28 14:05:59 -0400

    assets:precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,15 +75,15 @@ namespace :deploy do
     end
   end
 
-  namespace :assets do
-    desc "Precompile assets on local machine and upload them to the server."
-    task :precompile, roles: :web, except: {no_release: true} do
-      run_locally "bundle exec rake assets:precompile RAILS_ENV=development"
-      find_servers_for_task(current_task).each do |server|
-        run_locally "rsync -vr  --exclude='.DS_Store' --rsh 'ssh -p #{ssh_options[:port]}' public/assets  #{user}@a2s76.a2hosting.com:#{shared_path}/"
-      end
-    end
-  end
+  #namespace :assets do
+  #  desc "Precompile assets on local machine and upload them to the server."
+  #  task :precompile, roles: :web, except: {no_release: true} do
+  #    run_locally "bundle exec rake assets:precompile RAILS_ENV=development"
+  #    find_servers_for_task(current_task).each do |server|
+  #      run_locally "rsync -vr  --exclude='.DS_Store' --rsh 'ssh -p #{ssh_options[:port]}' public/assets  #{user}@a2s76.a2hosting.com:#{shared_path}/"
+  #    end
+  #  end
+  #end
 
 
 end
