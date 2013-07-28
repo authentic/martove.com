3c83fb6d819a7de6b275fc7418c8756950c4b3c6
authentic <galaxy73rambler@yahoo.com>
2013-07-27 21:42:39 -0400

    application.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,18 +64,6 @@ MartoveCom::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.assets.precompile += ['portfolio.css']
+  config.assets.precompile += ['jquery.isotope.min.js', 'isotope_custom.js', 'jquery.mousewheel-3.0.6.pack', 'jquery.fancybox.pack.js', 'jquery.fancybox-media.js', 'fancybox_init.js', 'jquery.fancybox-thumbs.js', 'jquery.fancybox-buttons.js']
 
-  config.assets.precompile += ['jquery.fancybox-buttons.css']
-  config.assets.precompile += ['jquery.fancybox-thumbs.css']
-  config.assets.precompile += ['hero-equal-thumb-gallery.css']
-  config.assets.precompile += ['jquery.isotope.min.js']
-  config.assets.precompile += ['isotope_custom.js']
-  config.assets.precompile += ['jquery.mousewheel-3.0.6.pack.js']
-  config.assets.precompile += ['jquery.fancybox.pack.js']
-  config.assets.precompile += ['jquery.fancybox-media.js']
-  config.assets.precompile += ['jquery.fancybox-buttons.js']
-  config.assets.precompile += ['jquery.fancybox-thumbs.js']
-  config.assets.precompile += ['fancybox_init.js']
-  config.assets.precompile += ['jquery.fancybox.css']
 end
